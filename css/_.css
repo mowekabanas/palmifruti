bc84c063d6250eb8b530450c3f60a8de2cb9ad6a
dudusbarros <joseeduardobarros@gmail.com>
2016-05-06 10:05:28 -0300

    Init semantic structure Init index.html Init _.js header Init _.css header

--- css/_.css
+++ css/_.css
@@ -1,6 +1,6 @@
 /*!
- * Mowe {ProjectName} Project v0.0.0 (http://letsmowe.org/)
- * Copyright 2013-2015 Mowe Developers
- * Licensed under MIT (https://github.com/mowekabanas/base/blob/master/LICENSE)
+ * Mowe PalmiFruti Project v0.0.0 (http://letsmowe.org/)
+ * Copyright 2013-2016 Mowe Developers
+ * Licensed under MIT (https://github.com/mowekabanas/palmifruti/blob/master/LICENSE)
 */
 
